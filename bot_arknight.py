1cc88a1b0489f2429a3c1b5cf06be6e44524b96d
DirecterX <58391831+DirecterX@users.noreply.github.com>
2020-05-12 20:56:21 +0700

    Add files via upload

---
+++ bot_arknight.py
@@ -0,0 +1,44 @@
+import pyautogui
+import time
+time.sleep(1)
+#declare Variable
+play = True
+border = [600,0,1320,755]
+while play: 
+    
+    try:
+        start_time = time.time()
+        #--------------------Start Stage---------------------------
+        if pyautogui.locateOnScreen('arknight/start.png',region=(border),confidence=0.9)!=None:               
+            pyautogui.click(pyautogui.locateOnScreen('arknight/start.png',region=(border),confidence=0.9))
+            print("Stage Selected")
+            time.sleep(1)
+        #-------------------Start Mission--------------------------    
+        elif pyautogui.locateOnScreen('arknight/msstart.png',region=(border),confidence=0.9)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/msstart.png',region=(border),confidence=0.9))
+            print("Start Mission")
+            time.sleep(5)
+        #---------------------Stage Select-------------------------
+        elif pyautogui.locateOnScreen('arknight/stage.png',region=(border),confidence=0.94)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/stage.png',region=(border),confidence=0.94))
+            print("Select Stage")
+            time.sleep(1)
+        #----------------------End Stage---------------------------        
+        elif pyautogui.locateOnScreen('arknight/result.png',region=(border),confidence=0.9)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/result.png',region=(border),confidence=0.9))
+            print("End Stage")
+            time.sleep(1)
+        #----------------------Doctar Level Up---------------------------   
+        elif pyautogui.locateOnScreen('arknight/levelup.png',region=(border),confidence=0.9)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/levelup.png',region=(border),confidence=0.9))
+            print("Doctar Level Up")
+            time.sleep(1)                   
+      
+
+    except Exception as e:
+        play = False
+        print(e)
+
+
+
+

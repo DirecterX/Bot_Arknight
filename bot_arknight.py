ac269d247abc95431bf3ce8d88fe6fdef81c787e
DirecterX <58391831+DirecterX@users.noreply.github.com>
2020-05-19 09:14:22 +0700

    Add files via upload

--- bot_arknight.py
+++ bot_arknight.py
@@ -13,9 +13,21 @@ while play:
             pyautogui.click(pyautogui.locateOnScreen('arknight/start.png',region=(border),confidence=0.9))
             print("Stage Selected")
             time.sleep(1)
+        elif pyautogui.locateOnScreen('arknight/obstart.png',region=(border),confidence=0.9)!=None:               
+            pyautogui.click(pyautogui.locateOnScreen('arknight/obstart.png',region=(border),confidence=0.9))
+            print("Stage Selected")
+            time.sleep(1)
+        elif pyautogui.locateOnScreen('arknight/cnstart.png',region=(border),confidence=0.9)!=None:               
+            pyautogui.click(pyautogui.locateOnScreen('arknight/cnstart.png',region=(border),confidence=0.9))
+            print("Stage Selected")
+            time.sleep(1)    
         #-------------------Start Mission--------------------------    
         elif pyautogui.locateOnScreen('arknight/msstart.png',region=(border),confidence=0.9)!=None:              
             pyautogui.click(pyautogui.locateOnScreen('arknight/msstart.png',region=(border),confidence=0.9))
+            print("Start Mission")
+            time.sleep(5)
+        elif pyautogui.locateOnScreen('arknight/cnmsstart.png',region=(border),confidence=0.9)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/cnmsstart.png',region=(border),confidence=0.9))
             print("Start Mission")
             time.sleep(5)
         #---------------------Stage Select-------------------------
@@ -23,9 +35,17 @@ while play:
             pyautogui.click(pyautogui.locateOnScreen('arknight/stage.png',region=(border),confidence=0.94))
             print("Select Stage")
             time.sleep(1)
+        elif pyautogui.locateOnScreen('arknight/off4.png',region=(border),confidence=0.94)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/off4.png',region=(border),confidence=0.94))
+            print("Select Stage")
+            time.sleep(1)
         #----------------------End Stage---------------------------        
         elif pyautogui.locateOnScreen('arknight/result.png',region=(border),confidence=0.9)!=None:              
             pyautogui.click(pyautogui.locateOnScreen('arknight/result.png',region=(border),confidence=0.9))
+            print("End Stage")
+            time.sleep(1)
+        elif pyautogui.locateOnScreen('arknight/cnresult.png',region=(border),confidence=0.9)!=None:              
+            pyautogui.click(pyautogui.locateOnScreen('arknight/cnresult.png',region=(border),confidence=0.9))
             print("End Stage")
             time.sleep(1)
         #----------------------Doctar Level Up---------------------------   
